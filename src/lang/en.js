d280edcfa9e470c6be30a2f1a6bfc5baa4fa5941
wade <745696658@qq.com>
2020-04-15 07:03:10 +0800

    2

---
+++ src/lang/en.js
@@ -0,0 +1,23 @@
+export default {
+  login: {
+    title1: 'Login',
+    title2: 'Form',
+    register: 'register',
+    phonePlaceHolder: 'Please enter phone number',
+    pwdPlaceHolder: 'Please enter password',
+    noAccountTip: 'No account?',
+    rememberPsd: 'Remember psw',
+    login: 'login'
+  },
+  register: {
+    title1: 'Register',
+    title2: 'Form',
+    register: 'register',
+    phonePlaceHolder: 'Please enter phone number',
+    pwdPlaceHolder: 'Please enter password',
+    CaptchaPlaceHolder: 'enter Captcha',
+    hasAccountTip: 'Existing Account?',
+    getCaptcha: 'get Captcha',
+    login: 'login'
+  }
+}
---
+++ src/lang/zh.js
@@ -0,0 +1,23 @@
+export default {
+  login: {
+    title1: '登',
+    title2: '录',
+    register: '注册',
+    phonePlaceHolder: '请输入手机号',
+    pwdPlaceHolder: '请输入密码',
+    noAccountTip: '没有账号?',
+    rememberPsd: '记住密码',
+    login: '登录'
+  },
+  register: {
+    title1: '注',
+    title2: '册',
+    register: '注册',
+    phonePlaceHolder: '请输入手机号',
+    pwdPlaceHolder: '请输入密码',
+    CaptchaPlaceHolder: '请输入验证码',
+    hasAccountTip: '已有账号?',
+    getCaptcha: '获取验证呀',
+    login: '登录'
+  }
+}
